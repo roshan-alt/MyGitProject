7a5dee4208983b4b923dcb516fc243ef8da56808
Roshan Adhikari <roshanadhikari0420@gmail.com>
2025-02-01 17:57:00 -0400

    Written push method for stack Implementation.

--- project.py
+++ project.py
@@ -1,1 +1,21 @@
-print("Hello Roshan")
+# Stack Implementation from group Member.
+"""
+Roshan: Make push method and len method.
+Rishi: Make pop method.
+Syed: Make peek method or make print method.
+"""
+class Stack:
+    def __init__(self):
+        self.arr = []
+        self.size = 0
+    
+    # Implementation of push method of Stack.
+    def push(self,item):
+        if (self.size+1 > len(self.arr)):
+            self.a.append(x)
+        else:
+            self.arr[self.size] = x
+        self.size+=1
+
+
+
